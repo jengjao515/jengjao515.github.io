bce3e3d12718ac2c07d4159b2e9af310d55c0de2
jengjao515 <jenjao515@gmail.com>
2015-05-12 22:11:34 -0700

    testing

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="testindex.css"/>
+        <title>jengablocks</title>
+    </head>
+    <body>
+
+    <!-- Footer will not stick to bottom page without wrapper -->
+        <div class="wrapper">
+
+			<main>
+				
+				<section>
+        	<nav>
+				<span class="topleft"><a href="#">jengablocks</a></span>
+				<span class="topright">
+						<a href="mailto:jenjao515@gmail.com"><img src="imgs/mail_mini.png" /></a>
+		&nbsp;&nbsp;	<a href="#"><img src="imgs/linkedin_mini.png" /></a>
+		&nbsp;&nbsp;	<a href="#"><img src="imgs/fb_mini.png" /></a>
+		&nbsp;&nbsp;	<a href="#"><img src="imgs/twitter_mini.png" /></a>
+		&nbsp;&nbsp;	<a href="#"><img src="imgs/insta_mini.png" /></a>
+		&nbsp;&nbsp;	<a href="#"><img src="imgs/github_mini.png" /></a>
+				</span>
+			</nav>
+
+			<header>
+				"Anything is possible if you've got enough nerve." <br/> - J.K. Rowling
+	
+				<table class="tablemenu">
+					<tr>
+						<td class="menutitle">
+							menu
+					</td>
+					<td class="menu">
+						<a href="#">blog</a>
+						<a href="#">projects</a>
+						<a href="#">bio</a>
+						<a href="#">learn</a>
+					</td>
+					</tr>
+				</table>
+			</header>
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+include('header.php');
+echo
+				<h1>hello! where am i? </h1>
+	
+				Welcome to <b>jengablocks</b>, internet home of Jennifer Jaochico, a hopeful developer, tv enthusiast, reader, and gamer. 
+				</section>
+
+			</main>
+
+			<div class="push"></div>
+
+		</div>
+
+       <footer class="footer">
+            <p>design by jennifer jaochico</p>
+        </footer>
+
+    </body>
+</html>
+
+
+
+
+
+?>
